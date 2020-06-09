fb7708378d6a424b7e3007aa52e4382a59c9a712
ruofu <ruofuwan@usc.edu>
2020-06-09 13:55:37 -0700

    Implement POC door system

---
+++ POC/DoorSystem/Assets/GameEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    public void Awake()
+    {
+        current = this;
+    }
+
+    private Dictionary<int, List<int>> scenesDoors = new Dictionary<int, List<int>>();
+    
+    public event Action<int> onDoorwayTriggerEnter;
+    public void DoorwayTriggerEnter(int id)
+    {
+        if (onDoorwayTriggerEnter != null && KeyManagement.current.hasKey(id))
+        {
+            KeyManagement.current.useKey(id);
+            onDoorwayTriggerEnter(id);
+        }
+    }
+}
---
+++ POC/DoorSystem/Assets/KeyManagement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManagement : MonoBehaviour
+{
+    public static KeyManagement current;
+
+    public void Awake()
+    {
+        current = this;
+    }
+
+    private Dictionary<int, int> ownKeys = new Dictionary<int, int>();
+
+    public void collectKey(int key)
+    {
+        if (!ownKeys.ContainsKey(key))
+            ownKeys[key] = 0;
+        ownKeys[key]++;
+    }
+    
+    public bool hasKey(int key)
+    {
+        return (ownKeys.ContainsKey(key) && ownKeys[key] > 0);
+    }
+
+    public void useKey(int key)
+    {
+        ownKeys[key]--;
+    }
+}

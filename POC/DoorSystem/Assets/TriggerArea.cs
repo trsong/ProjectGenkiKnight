20729f45e0b1cd17aa2af3d69e19be2542fe0d2e
Ruofu Wang <31981600+yeukfu@users.noreply.github.com>
2020-06-09 19:28:18 -0700

    Implement POC door system (#7)
    Implement POC door system. The current version has following features:
    
    Open(doorID).
    Auto open door.
    Locked door, collect key, use key to open a door.

---
+++ POC/DoorSystem/Assets/TriggerArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    public int id;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameEvents.current.DoorwayTriggerEnter(id);
+    }
+}

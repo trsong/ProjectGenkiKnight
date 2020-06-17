d79a9a3d3f468446a9d38716af0e693fda89f80a
Bo <1445645878@qq.com>
2020-06-17 12:32:09 -0700

    clean up project forder

---
+++ Genki/Assets/Scripts/DoorEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEvents : MonoBehaviour
+{
+    public static DoorEvents current;
+
+    public void Awake()
+    {
+        current = this;
+    }
+
+    private Dictionary<int, List<int>> scenesDoors = new Dictionary<int, List<int>>();
+    
+    public event Action<int> onDoorwayTriggerEnter;
+    public void DoorwayTriggerEnter(int id)
+    {
+        if (onDoorwayTriggerEnter != null && DoorKeyManagement.current.hasKey(id))
+        {
+            DoorKeyManagement.current.useKey(id);
+            onDoorwayTriggerEnter(id);
+        }
+    }
+}
---
+++ Genki/Assets/Scripts/DoorKey.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+    public int id;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DoorKeyManagement.current.collectKey(id);
+        Object.Destroy(this.gameObject);
+    }
+}

ef3a93e203319bdbe6f0f6d2fc88604cc6e03103
ruofu <ruofuwan@usc.edu>
2020-06-30 21:20:59 -0700

    Add joystick

--- Genki/Assets/Scripts/Control/PlayerControl.cs
+++ Genki/Assets/Scripts/Control/PlayerControl.cs
@@ -8,6 +8,8 @@ namespace Genki.Control
     public class PlayerControl : BaseUnitControl
     {
         public Transform firePoint;
+        public Joystick joyStick;
+        public JoyButton joyButton;
 
         void Update()
         {
@@ -17,13 +19,14 @@ namespace Genki.Control
 
         void move()
         {
-            float xMove = Input.GetAxis("Horizontal");
-            float yMove = Input.GetAxis("Vertical");
+            float xMove = Input.GetAxis("Horizontal") + joyStick.Horizontal;
+            float yMove = Input.GetAxis("Vertical") + joyStick.Vertical;
             characterSystem.moveCharacter(xMove, yMove);
         }
 
         void shoot(){
-            if(Input.GetButtonDown("Fire1")){
+            // if(Input.GetButtonDown("Fire1") || joyButton.pressed){
+            if(joyButton.pressed){
                 weaponSystem.shoot(firePoint);
             }
         }
---
+++ Genki/Assets/Scripts/JoyButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public bool pressed;
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+}

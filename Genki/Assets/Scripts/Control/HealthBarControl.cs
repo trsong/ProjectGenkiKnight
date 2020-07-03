81b723e34e095e0153ad85197d24750963aa5559
trsong <trsong.cs@gmail.com>
2020-07-03 02:19:31 -0700

    Add Health Bar for each enemy prefab

--- Genki/Assets/Scripts/Control/HealthBarControl.cs
+++ Genki/Assets/Scripts/Control/HealthBarControl.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Genki.Control
 {
     public class HealthBarControl : MonoBehaviour
     {
         float healthPercentage = 1.0f;
+        private Vector2 originalScale = Vector2.one;
+
+        private void Start()
+        {
+            originalScale = transform.GetChild(0).localScale;
+        }
 
         void Update()
         {
-            transform.GetChild(0).localScale = new Vector3(healthPercentage, 1, 1);
+            transform.GetChild(0).localScale = new Vector3(originalScale.x * healthPercentage, originalScale.y, 1);
         }
 
         public void setHealth(float updatedHealth)

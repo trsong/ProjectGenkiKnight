0ff6c57851112ac4078c75daf64819a4db0ad5be
Bo <1445645878@qq.com>
2020-07-04 16:20:51 -0700

    finished start scene

---
+++ Genki/Assets/Scripts/UI/SceneSwitchButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitchButton : MonoBehaviour
+{
+    public Animator transition;
+    public void SwitchScene ()
+    {
+        StartCoroutine(switchNext());
+    }
+    IEnumerator switchNext(){
+        transition.SetTrigger("end");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(1);
+    }
+}

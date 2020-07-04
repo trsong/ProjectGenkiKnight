93202f45c4f1382370d99892520eb113c000a1d2
Yicheng Song <sdfqb009@gmail.com>
2020-07-04 19:33:41 -0400

    enemy movement changed, add a new script called enemySystem

--- Genki/Assets/Scripts/Control/EnemyControl.cs
+++ Genki/Assets/Scripts/Control/EnemyControl.cs
@@ -5,104 +5,13 @@ namespace Genki.Control
 {
     public class EnemyControl : BaseUnitControl
     {
-        public Animator a;
-        public Transform enemyPos;
-        public int minRange;
-        public int maxRange;
-        public int attackRange;
-        public GameObject attackTarget = null;
-
-        private int Timer = 0;
         
         protected override void Start()
         {
             base.Start();
-            if (attackTarget == null)
-            {
-                attackTarget = GameObject.FindWithTag("Player");
-            }
+            
         }
         
         
-        void Update()
-        {
-            Timer++;
-            a = GetComponent<Animator>();
-            if (attackTarget == null)
-            {
-                attackTarget = GameObject.FindWithTag("Player");
-            }
-            if (Vector3.Distance(attackTarget.transform.position, transform.position) <= maxRange && Vector3.Distance(attackTarget.transform.position, transform.position) >= minRange)
-            {
-                if (Vector3.Distance(attackTarget.transform.position, transform.position) <= attackRange)
-                {
-                    if (Timer % 150 == 0)
-                    {
-                        attack();
-                    }
-                }
-                else
-                {
-                    followPlayer();
-                }
-            }
-            else if (Vector3.Distance(attackTarget.transform.position, transform.position) > maxRange)
-            {
-                goBack();
-            }
-        }
-
-        public void followPlayer()
-        {
-            a.SetBool("isAttacking", false);
-            a.SetBool("isMoving", true);
-            Vector2 movePos = Correction((attackTarget.transform.position.x - transform.position.x), (attackTarget.transform.position.y - transform.position.y));
-            characterSystem.moveCharacter(movePos);
-        }
-
-        public void goBack()
-        {
-            a.SetBool("isAttacking", false);
-            a.SetBool("isMoving", true);
-            Vector2 movePos = Correction((enemyPos.position.x - transform.position.x), (enemyPos.position.y - transform.position.y));
-            characterSystem.moveCharacter(movePos);
-            if (Vector3.Distance(enemyPos.position, transform.position) == 0)
-            {
-                a.SetBool("isMoving", false);
-            }
-        }
-        public void attack()
-        {
-            a.SetBool("isMoving", false);
-            a.SetBool("isAttacking", true);
-            Vector2 movePos = Correction((attackTarget.transform.position.x - transform.position.x), (attackTarget.transform.position.y - transform.position.y));
-            characterSystem.moveCharacter(movePos);
-            weaponSystem.shootTarget(attackTarget.transform);
-        }
-        public Vector2 Correction(float x, float y)
-        {
-            Vector2 result;
-            if ((y - x <= 0) && (y + x > 0))
-            {
-                result.x = 1;
-                result.y = 0;
-            }
-            else if ((y - x > 0) && (y + x > 0))
-            {
-                result.x = 0;
-                result.y = 1;
-            }
-            else if ((y - x > 0) && (y + x <= 0))
-            {
-                result.x = -1;
-                result.y = 0;
-            }
-            else
-            {
-                result.x = 0;
-                result.y = -1;
-            }
-            return result;
-        }
     }
 }

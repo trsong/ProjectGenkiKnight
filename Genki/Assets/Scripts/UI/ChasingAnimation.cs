09f5cddb63d29433a9eaeead290c7e79f8db7ed9
Bo <1445645878@qq.com>
2020-07-04 19:59:59 -0700

    finished minimap

--- Genki/Assets/Scripts/UI/ChasingAnimation.cs
+++ Genki/Assets/Scripts/UI/ChasingAnimation.cs
@@ -4,51 +4,51 @@ using UnityEngine;
 
 public class ChasingAnimation : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject chicken;
+    public GameObject chaser;
+    public GameObject runner;
     private Vector2 ChickenMove = new Vector2(0,0),PLayerMove = new Vector2(0,0);
     private float speed = 5,dis = 0.1f;
     void Update()
     {
-        chicken.transform.Translate(ChickenMove * Time.deltaTime *speed);
-        player.transform.Translate(PLayerMove * Time.deltaTime*speed);
-        if(Vector3.Distance(chicken.transform.position , new Vector3(-7,3,0)) < dis ){
-            chicken.GetComponent<Animator>().SetFloat("moveY",0);
-            chicken.GetComponent<Animator>().SetFloat("moveX",1);
+        runner.transform.Translate(ChickenMove * Time.deltaTime *speed);
+        chaser.transform.Translate(PLayerMove * Time.deltaTime*speed);
+        if(Vector3.Distance(runner.transform.position , new Vector3(-7,3,0)) < dis ){
+            runner.GetComponent<Animator>().SetFloat("moveY",0);
+            runner.GetComponent<Animator>().SetFloat("moveX",1);
             ChickenMove = Vector2.right;
         }
-        if(Vector3.Distance(chicken.transform.position , new Vector3(7,3,0)) < dis ){
-            chicken.GetComponent<Animator>().SetFloat("moveY",-1);
-            chicken.GetComponent<Animator>().SetFloat("moveX",0);
+        if(Vector3.Distance(runner.transform.position , new Vector3(7,3,0)) < dis ){
+            runner.GetComponent<Animator>().SetFloat("moveY",-1);
+            runner.GetComponent<Animator>().SetFloat("moveX",0);
             ChickenMove = Vector2.down;
         }
-        if(Vector3.Distance(chicken.transform.position , new Vector3(7,-3,0)) < dis ){
-            chicken.GetComponent<Animator>().SetFloat("moveX",-1);
-            chicken.GetComponent<Animator>().SetFloat("moveY",0);
+        if(Vector3.Distance(runner.transform.position , new Vector3(7,-3,0)) < dis ){
+            runner.GetComponent<Animator>().SetFloat("moveX",-1);
+            runner.GetComponent<Animator>().SetFloat("moveY",0);
             ChickenMove = Vector2.left;
         }
-        if(Vector3.Distance(chicken.transform.position , new Vector3(-7,-3,0)) < dis ){
-            chicken.GetComponent<Animator>().SetFloat("moveY",1);
-            chicken.GetComponent<Animator>().SetFloat("moveX",0);
+        if(Vector3.Distance(runner.transform.position , new Vector3(-7,-3,0)) < dis ){
+            runner.GetComponent<Animator>().SetFloat("moveY",1);
+            runner.GetComponent<Animator>().SetFloat("moveX",0);
             ChickenMove = Vector2.up;
-        }if(Vector3.Distance(player.transform.position , new Vector3(-7,-3,0)) < dis ){
-            player.GetComponent<Animator>().SetFloat("moveX",0);
-            player.GetComponent<Animator>().SetFloat("moveY",1);
+        }if(Vector3.Distance(chaser.transform.position , new Vector3(-7,-3,0)) < dis ){
+            chaser.GetComponent<Animator>().SetFloat("moveX",0);
+            chaser.GetComponent<Animator>().SetFloat("moveY",1);
             PLayerMove = Vector2.up;
         }
-        if(Vector3.Distance(player.transform.position , new Vector3(-7,3,0)) < dis ){
-            player.GetComponent<Animator>().SetFloat("moveY",0);
-            player.GetComponent<Animator>().SetFloat("moveX",1);            
+        if(Vector3.Distance(chaser.transform.position , new Vector3(-7,3,0)) < dis ){
+            chaser.GetComponent<Animator>().SetFloat("moveY",0);
+            chaser.GetComponent<Animator>().SetFloat("moveX",1);            
             PLayerMove = Vector2.right;
         }
-        if(Vector3.Distance(player.transform.position , new Vector3(7,3,0)) < dis ){
-            player.GetComponent<Animator>().SetFloat("moveX",0);
-            player.GetComponent<Animator>().SetFloat("moveY",-1);            
+        if(Vector3.Distance(chaser.transform.position , new Vector3(7,3,0)) < dis ){
+            chaser.GetComponent<Animator>().SetFloat("moveX",0);
+            chaser.GetComponent<Animator>().SetFloat("moveY",-1);            
             PLayerMove = Vector2.down;
         }
-        if(Vector3.Distance(player.transform.position , new Vector3(7,-3,0)) < dis ){
-            player.GetComponent<Animator>().SetFloat("moveY",0);
-            player.GetComponent<Animator>().SetFloat("moveX",-1);            
+        if(Vector3.Distance(chaser.transform.position , new Vector3(7,-3,0)) < dis ){
+            chaser.GetComponent<Animator>().SetFloat("moveY",0);
+            chaser.GetComponent<Animator>().SetFloat("moveX",-1);            
             PLayerMove = Vector2.left;
         }
     }

714e2352661ec9e2b7dba5505d79d6466aee5599
Bo <1445645878@qq.com>
2020-07-06 16:33:43 -0700

    add loot

---
+++ Genki/Assets/Scripts/Loot/LootDrop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootDrop : MonoBehaviour
+{
+    public List<GameObject>  drops;
+    public Sprite openLoot;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.transform.tag=="Player"){
+            Destroy(this.gameObject.GetComponent<Collider2D>());
+            Destroy(this.transform.GetChild(0).gameObject);
+            SpriteRenderer spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = openLoot;
+            Color temp = spriteRenderer.color;
+            temp.a = 0.5f;
+            spriteRenderer.color = temp;
+        }
+    }
+}

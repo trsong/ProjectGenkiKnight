d512bb3983ae7b94509939936adf84a301a2c246
ruofu <ruofuwan@usc.edu>
2020-07-06 21:48:19 -0700

    Add audio trigger

---
+++ Genki/Assets/Scripts/Audio/AudioTriggerArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggerArea : MonoBehaviour
+{
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource.Stop();
+        }
+    }
+}

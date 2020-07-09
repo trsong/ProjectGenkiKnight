c8117b7944e4d39758cdb3408c37ce21dc7f733f
trsong <trsong.cs@gmail.com>
2020-07-09 00:35:42 -0700

    Add Enemy Trap

---
+++ Genki/Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTrigger : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.transform.CompareTag("Player")) return;
+        Destroy(gameObject.GetComponent<Collider2D>());
+        
+        for (int j = 0; j < transform.childCount; j++)
+        {
+            transform.GetChild(j).gameObject.SetActive(true);
+        }
+    }
+}

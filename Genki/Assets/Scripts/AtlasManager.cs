82e26b4b71ef1cf3199ce9249eca029e11419165
trsong <trsong.cs@gmail.com>
2020-07-13 15:31:30 -0700

    Import Tutorial Dialog Scene

---
+++ Genki/Assets/Scripts/AtlasManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AtlasManager : MonoBehaviour, IManager
+{
+    public static Sprite[] sprites;
+    public ManagerState currentState { get; private set; }
+    public void BootSequence()
+    {
+        Debug.Log(string.Format("{0} is booting up", GetType().Name));
+        sprites = Resources.LoadAll<Sprite>("EventAtlas");
+        currentState = ManagerState.Completed;
+        Debug.Log(string.Format("{0} status = {1}", GetType().Name, currentState));
+    }
+
+    public Sprite loadSprite(string spriteName)
+    {
+        foreach (Sprite s in sprites)
+        {
+            if (s.name == spriteName)
+            {
+                return s;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ Genki/Assets/Scripts/IManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ManagerState
+{
+    Offline, Initializing, Completed
+}
+public interface IManager
+{
+    ManagerState currentState { get; }
+    void BootSequence();
+}

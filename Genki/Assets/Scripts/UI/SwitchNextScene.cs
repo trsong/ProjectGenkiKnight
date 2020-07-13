d3affa90f3b104baccf5ef7e22aa1d9059e80f4d
Bo <1445645878@qq.com>
2020-07-13 11:06:33 -0700

    loadNextScene script done

---
+++ Genki/Assets/Scripts/UI/SwitchNextScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchNextScene : MonoBehaviour
+{
+    public Animator transition;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player"){
+            StartCoroutine(NextScene());
+        }
+    }
+
+    IEnumerator NextScene()
+    {
+        transition.SetTrigger("end");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
